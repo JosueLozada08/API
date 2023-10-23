acb00334bb4afaf2eea6035eeedeaafdce5526fd
josue <josue_lozadavillacis@hotmail.com>
2023-10-23 07:55:00 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,98 @@
+﻿using APIProductos.Data;
+using APIProductos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIProductos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public ProductoController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Producto> products = await _db.Productos.ToListAsync();
+            return Ok(products);
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{IdProducto}")]
+        public async Task<IActionResult> Get(int IdProducto)
+        {
+            Producto producto = await _db.Productos.FirstOrDefaultAsync(x=>x.IdProducto == IdProducto);
+            if(producto == null) {
+                return BadRequest();
+            }
+            return Ok(producto);
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Producto producto)
+        {
+            Producto pEncontrado = await _db.Productos.FirstOrDefaultAsync(x => x.IdProducto == producto.IdProducto);
+
+            if( pEncontrado == null && producto != null)
+            {
+
+                await _db.Productos.AddAsync(producto);
+                await _db.SaveChangesAsync();
+                return Ok(producto);
+
+            }
+
+            return BadRequest();
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{IdProductos}")]
+        public async Task<IActionResult> Put(int IdProductos, [FromBody] Producto producto)
+        {
+
+            Producto pEncontrado = await _db.Productos.FirstOrDefaultAsync(x => x.IdProducto == producto.IdProducto);
+
+            if (pEncontrado != null)
+            {
+                pEncontrado.Nombre = producto.Nombre != null ? producto.Nombre : pEncontrado.Nombre;
+                pEncontrado.Descripcion = producto.Descripcion != null ? producto.Descripcion : pEncontrado.Descripcion;
+                pEncontrado.Cantidad = producto.Cantidad != null ? producto.Cantidad : pEncontrado.Cantidad;
+                _db.Update(pEncontrado);
+                await _db.SaveChangesAsync();
+                return Ok(pEncontrado);
+            }
+
+
+            return BadRequest();
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{IdProducto}")]
+        public async Task<IActionResult> Delete(int IdProducto)
+        {
+
+            Producto pEncontrado = await _db.Productos.FirstOrDefaultAsync(x => x.IdProducto == IdProducto);
+
+
+            if (pEncontrado != null)
+            {
+                _db.Productos.Remove(pEncontrado);
+                await _db.SaveChangesAsync();
+                return NoContent(); 
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,51 @@
+﻿using APIProductos.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace APIProductos.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        { 
+
+
+        
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().HasData(
+
+                new Producto
+                {
+                    IdProducto = 1,
+                    Nombre = "Producto 1",
+                    Descripcion = "Desc 1",
+                    Cantidad = 34
+                },
+
+                new Producto
+                {
+                    IdProducto = 2,
+                    Nombre = "Producto 2",
+                    Descripcion = "Desc 2",
+                    Cantidad = 34
+                },
+
+                new Producto
+                {
+                    IdProducto = 3,
+                    Nombre = "Producto 3",
+                    Descripcion = "Desc 3",
+                    Cantidad = 3
+                }
+
+                );
+        }
+
+    }
+}
